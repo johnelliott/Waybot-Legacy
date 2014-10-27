c4e8a2e1560910caf9f35fff8b8bfb8349c3df3c
johnelliott <johnelliott703@gmail.com>
2014-10-27 13:01:12 -0400

    add indexes to User and dependent destroy relationships to User and Run models

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,3 +1,4 @@
 class Run < ActiveRecord::Base
   belongs_to :user
+  has_many :hits, dependent: :destroy
 end
--- db/migrate/20141027165257_create_users.rb
+++ db/migrate/20141027165257_create_users.rb
@@ -7,5 +7,7 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
+    add_index :users, :email, unique: true
+    add_index :users, :remember_token
   end
 end
