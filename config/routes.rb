4b11c572f14b1f83b5909bd9efb451a299d84f48
johnelliott <johnelliott703@gmail.com>
2014-10-29 10:40:49 -0400

    remove view listing all users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :runs
-
-  resources :users
+  resources :users, except: :index
+  root 'runs#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
