eaea1d82de1849625d82625e721658bbd0f7177a
johnelliott <johnelliott703@gmail.com>
2014-10-29 21:34:57 -0400

    change runs view to look more like goal

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,4 +1,10 @@
 class Run < ActiveRecord::Base
+  include ActionView::Helpers::DateHelper
   belongs_to :user
   has_many :hits, dependent: :destroy
+  def elapsed_time
+    thestart = self.start_time
+    theend = self.end_time
+    distance_of_time_in_words(thestart, theend)
+  end
 end
