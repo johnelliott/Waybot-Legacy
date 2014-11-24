6ade70bc39f4331ea93a5c4a220319943252f82a
johnelliott <johnelliott703@gmail.com>
2014-11-23 23:28:40 -0500

    experiment with chartkick and activerecord

--- app/models/run.rb
+++ app/models/run.rb
@@ -7,4 +7,7 @@ class Run < ActiveRecord::Base
     theend = self.end_time
     distance_of_time_in_words(thestart, theend)
   end
+  def speeds_count_chart
+    self.hits.group(:speed).count
+  end
 end
