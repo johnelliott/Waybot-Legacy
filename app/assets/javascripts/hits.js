341a33a67c2980cf3aa24ed0ff06989651586a4b
johnelliott <johnelliott703@gmail.com>
2014-12-16 12:38:40 -0500

    add some code to test backbone models

--- app/assets/javascripts/hits.js
+++ app/assets/javascripts/hits.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-
+// Waybot-node subscriber
 console.log('hello from the hits.js file in ruby');
 
 window.client = new Faye.Client('http://localhost:3001/hits', {
@@ -11,4 +11,41 @@ window.client = new Faye.Client('http://localhost:3001/hits', {
 
 var subscription = window.client.subscribe('/hits', function(message) {
     console.log(message);
+    hits.storeHit(message, function(){
+        console.log("hit stored");
+    });
+});
+// end subscriber
+
+// Backbone model
+
+// Hit Data Model: 
+var Hit = Backbone.Model.extend({
+  defaults: {
+    run_id: '',
+    time: '',
+    speed: ''
+  }
+});
+
+// do I even need a collection?
+// Run Models Collection:
+var HitCollection = Backbone.Collection.extend({
+  model: Hit,
+  url: '/hits',
+  storeHit: function(message, callback){
+    var json = JSON.parse(message);
+    // do some other things to save the time and speed into the model
+    callback();
+  }
+});
+
+// Create collection and (not a, for now) router:
+var hits = new HitCollection();
+//var router = new hitsRouter({collection: hits});
+
+// Fetch collection, and then start history:
+hits.fetch().then(function() {
+  Backbone.history.start();
+  console.log('hello from hits collection fetch callback');
 });
